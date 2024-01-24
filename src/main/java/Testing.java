a7d94a10e1ece6ca744e6b2ef17adc6a495f97c9
Matthew <12hughesm@gmail.com>
2024-01-24 15:38:54 +0000

    tested and fixed the InputManager and InputValidator

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,5 +1,12 @@
+import utils.InputManager;
+import utils.InputValidator;
+
 public class Testing {
     public static void main(String[] args) {
 
+        InputManager input = new InputManager();
+        String str = input.getStringInput();
+
+        System.out.println(str);
     }
 }
--- src/main/java/utils/InputManager.java
+++ src/main/java/utils/InputManager.java
@@ -1,5 +1,37 @@
 package utils;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputManager {
+    private final Scanner SCANNER = new Scanner(System.in);
+
+    public int getIntegerInput(int min, int max){
+        while(true){
+            try{
+                int userInput = SCANNER.nextInt();
+                if(!InputValidator.isValidIntInput(userInput, min, max))
+                    throw new InputMismatchException(String.format("Number not in range of %d to %d", min, max));
+                SCANNER.nextLine();
+                return userInput;
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                SCANNER.nextLine();
+            }
+        }
+    }
+    public String getStringInput(){
+        while(true){
+            try{
+                String userInput = SCANNER.nextLine();
+                if(userInput.isBlank()) throw new InputMismatchException("Input cannot be blank");
+                SCANNER.nextLine();
+                return userInput;
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                SCANNER.nextLine();
+            }
+        }
+    }
 
 }
