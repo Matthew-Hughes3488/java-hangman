a25dbcd5fff68d93c873e474952107daeea5a852
Matthew <12hughesm@gmail.com>
2024-01-24 17:16:13 +0000

    Refactored GuessHandler fields

--- src/main/java/utils/GuessHandler.java
+++ src/main/java/utils/GuessHandler.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class GuessHandler {
     //Store the current word being used in the game
-    private String word;
+    private GameWord gameWord;
     //store all guess
     private Set<String> guessedChars;
     //store how much of the word they have got so far
@@ -16,6 +16,6 @@ public class GuessHandler {
     //check if word contain the guess
     //check if the word has been successfully guessed
     private boolean wordGuessed(){
-        return word.length() == correctGuesses;
+        return gameWord.getWord().length() == correctGuesses;
     }
 }
