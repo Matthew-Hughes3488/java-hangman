47cfb1e7c7200d3515aec91f85929ebbfe63893f
Matthew <12hughesm@gmail.com>
2024-01-31 14:29:02 +0000

    Reformatted code

--- src/main/java/game/GameLauncher.java
+++ src/main/java/game/GameLauncher.java
@@ -1,8 +1,6 @@
 package game;
 
-import utils.InputManager;
 import utils.InputProvider;
-import utils.OutputManager;
 import utils.OutputProvider;
 
 public class GameLauncher {
@@ -28,24 +26,26 @@ public class GameLauncher {
         this.gameSettings = gameSettings;
         onLaunch();
     }
+
     /**
      * Launches the game by printing the game launch message and difficulty message,
      * and entering the main loop.
      */
-    public void onLaunch(){
+    public void onLaunch() {
         gameOutputProvider.printGameLaunchMessage();
         gameOutputProvider.printDifficultyMessage();
         mainLoop();
     }
+
     /**
      * Enters the main loop of the game, handling user input and executing game actions
      * based on the input.
      */
-    private void mainLoop(){
+    private void mainLoop() {
         gameOutputProvider.printCommands();
         int userInput = inputProvider.getIntegerInput(0, gameOutputProvider.getNumCommands() - 1);
 
-        switch (userInput){
+        switch (userInput) {
             case 0:
                 startNewGame();
                 break;
@@ -58,22 +58,24 @@ public class GameLauncher {
 
         mainLoop();
     }
+
     /**
      * Starts a new game by creating a GameManager object with the specified number
      * of tries from the game settings, and starting the game.
      */
-    private void startNewGame(){
+    private void startNewGame() {
         GameManager game = new GameManager(gameSettings.getNumberOfTries());
-        if(game.startGame()) startNewGame();
+        if (game.startGame()) startNewGame();
     }
+
     /**
      * Changes the game settings based on user input.
      */
-    private void changeSettings(){
+    private void changeSettings() {
         settingsOutputProvider.printCommands();
         int userInput = inputProvider.getIntegerInput(0, settingsOutputProvider.getNumCommands() - 1);
 
-        switch (userInput){
+        switch (userInput) {
             case 0:
                 gameSettings.setDifficulty("easy");
                 break;
